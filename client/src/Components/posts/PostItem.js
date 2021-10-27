38d800bf0c22c4426a9af45af8228fdbfaf2a0f8
isharrma <54413011+isharrma@users.noreply.github.com>
2021-10-28 04:14:15 +0530

    Completed,just few bags

--- client/src/Components/posts/PostItem.js
+++ client/src/Components/posts/PostItem.js
@@ -10,50 +10,60 @@ import {
   AiFillDelete,
 } from "react-icons/ai";
 
-import { addLike, removeLike } from "../../actions/post";
+import { addLike, removeLike, deletePost } from "../../actions/post";
 
 const PostItem = ({
   addLike,
   removeLike,
+  deletePost,
   auth,
+  showActions,
   post: { _id, text, username, avatar, user, likes, comments, date },
 }) => {
   return (
     <div class="posts">
       <div class="post bg-white p-1 my-1">
         <div>
-          <a href="profile.html">
+          <Link to={`/profile/${user}`}>
             <img class="round-img" src={avatar} alt="" />
             <h4>{username}</h4>
-          </a>
+          </Link>
         </div>
         <div>
           <p class="my-1">{text}</p>
           <p class="post-date">
             Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
           </p>
-          <button
-            type="button"
-            class="btn btn-light"
-            onClick={(e) => addLike(_id)}
-          >
-            <AiFillLike /> <span>{likes.length}</span>
-          </button>
-          <button
-            type="button"
-            class="btn btn-light"
-            onClick={(e) => removeLike(_id)}
-          >
-            <AiFillDislike />{" "}
-          </button>
-          <Link to={`/post/${_id}`} class="btn btn-primary">
-            <AiOutlineComment />
-            <span class="comment-count">{comments.length}</span>
-          </Link>
-          {!auth.laoding && user === auth.user._id && (
-            <button type="button" class="btn btn-danger">
-              <AiFillDelete />
-            </button>
+          {showActions && (
+            <Fragment>
+              <button
+                type="button"
+                class="btn btn-light"
+                onClick={(e) => addLike(_id)}
+              >
+                <AiFillLike /> <span>{likes.length}</span>
+              </button>
+              <button
+                type="button"
+                class="btn btn-light"
+                onClick={(e) => removeLike(_id)}
+              >
+                <AiFillDislike />{" "}
+              </button>
+              <Link to={`/post/${_id}`} class="btn btn-primary">
+                <AiOutlineComment />
+                <span class="comment-count">{comments.length}</span>
+              </Link>
+              {!auth.laoding && user === auth.user._id && (
+                <button
+                  type="button"
+                  class="btn btn-danger"
+                  onClick={(e) => deletePost(_id)}
+                >
+                  <AiFillDelete />
+                </button>
+              )}
+            </Fragment>
           )}
         </div>
       </div>
@@ -61,13 +71,22 @@ const PostItem = ({
   );
 };
 
+PostItem.defaultProps = {
+  showActions: true,
+};
+
 PostItem.propTypes = {
   auth: PropTypes.object.isRequired,
   post: PropTypes.object.isRequired,
+  addLike: PropTypes.func.isRequired,
+  removeLike: PropTypes.func.isRequired,
+  deletePost: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   auth: state.auth,
 });
 
-export default connect(mapStateToProps, { addLike, removeLike })(PostItem);
+export default connect(mapStateToProps, { addLike, removeLike, deletePost })(
+  PostItem
+);
